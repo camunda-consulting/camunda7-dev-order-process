6142d9b8a1c5efd773b42cbe68bdf7c15c9f0e9b
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2022-04-05 10:37:01 +0200

    buildable code

--- src/main/java/com/camunda/training/services/CreditCardService.java
+++ src/main/java/com/camunda/training/services/CreditCardService.java
@@ -1,6 +1,5 @@
 package com.camunda.training.services;
 
-import java.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,11 +12,6 @@ public class CreditCardService {
   public void chargeAmount(String cardNumber, String cvc, String expiryDate, Double amount) {
     LOG.info("charging card {} that expires on {} and has cvc {} with amount of {}", 
         cardNumber, expiryDate, cvc, amount);
-    
-    if (validateExpiryDate(expiryDate) == false) {
-      LOG.error("expiry date {} is invalid", expiryDate);
-      throw new IllegalArgumentException("invalid expiry date");
-    }
     
     LOG.info("payment completed");
   }
